c243f4ea634908e6b0de12f6c46bebc48e1f3f66
Christian van Gaalen <ctmvangaalen@gmail.com>
2022-11-03 20:34:16 +0100

    renamed output columns and updated .Rd file

--- R/dataDescribeR.R
+++ R/dataDescribeR.R
@@ -9,7 +9,7 @@
 #' @export
 #'
 #' @examples
-#' data_describer(mtdata)
+#' data_describer(tidyr::billboard)
 data_describer <- function(data) {
 
   # order the column names, so to reflect column order in source files
@@ -24,7 +24,7 @@ data_describer <- function(data) {
     tidyr::pivot_longer(cols = 1:ncol(data)) |>
     dplyr::filter(!is.na(value)) |>
     dplyr::group_by(name) |>
-    dplyr::sample_n(15) |>
+    dplyr::sample_n(5, replace = T) |>
     dplyr::summarise(
       value = stringr::str_c(value, collapse = "; ")
     ) |>
@@ -32,6 +32,11 @@ data_describer <- function(data) {
       name = factor(name, levels = ordered_cols)
     ) |>
     dplyr::arrange(name) |>
-    dplyr::left_join(skimr::skim(data), by = c("name" = "skim_variable"))
+    dplyr::left_join(skimr::skim(data), by = c("name" = "skim_variable")) |>
+    dplyr::rename(
+      "column_name" = 1,
+      "example_values" = 2,
+      "data_type_guess" = 3
+    )
 
 }
